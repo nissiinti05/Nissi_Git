4cf3e51aace8a9511332023ac72c6899fa87953b
nissiinti05 <121013155+nissiinti05@users.noreply.github.com>
2022-12-19 14:30:49 +0530

    Add files via upload

---
+++ Git.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Git
+{
+public static void main(String[]args)
+{
+int i,n;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+for(i=1;i<=10;i++)
+{
+System.out.println(n+"x"+i+"="+n*i);
+}
+}
+}
